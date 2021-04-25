4cd19cd699f4fd9a8ce0f778917f8a42578c5403
shabelson <shabelson@gmail.com>
2021-04-25 10:14:38 +0200

    1

--- Arduino/Recorder_Scoketcom_button/recorder_socketcom/recorder_socketcom.ino
+++ Arduino/Recorder_Scoketcom_button/recorder_socketcom/recorder_socketcom.ino
@@ -1,14 +1,12 @@
-#include <WiFi.h>
+#include "WiFi.h"
 #include <WiFiClient.h>
-
-#define SERVER_PORT 5000
 
 //char ssid[] =   "MiFibra-C9FA" ;
 //char pass[] =   "mrkKGDDF";
-/*
+
 char ssid[] =   "IAAC-WIFI" ;
 char pass[] =   "enteriaac2013";
-
+/*
 WiFiClient client;
 enum Protocol{
     PIN, 
@@ -19,23 +17,22 @@ enum Protocol{
 int IN_PIN =  2;
 
 void setup(){
-    //WiFi.mode(WIFI_STA)
-    //WiFi.begin(ssid, pass);
+    //WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, pass);
     Serial.begin(115200);
     pinMode(IN_PIN, INPUT);
     
-  /*
+  
     while (WiFi.status() != WL_CONNECTED){
       Serial.print(WiFi.status());
-      Serial.print("   ");
+      Serial.print(".");
       Serial.println(WL_CONNECTED);
-    */    delay(500);
+      delay(500);
     }
 
- 
+}
  
 void loop(){
-    
     /*if (!client.connect(WiFi.gatewayIP(), SERVER_PORT)){
         return;
     }
@@ -49,7 +46,5 @@ void loop(){
     client.flush();
     client.stop();
     */
-    Serial.println(digitalRead(IN_PIN));
-
-    
+    Serial.println(digitalRead(IN_PIN));   
 }
